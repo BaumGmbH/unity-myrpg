ee661c407371ebc160c151fdb17d496bc5a9c348
psyGamer <psygamer21@gmail.com>
2022-05-21 20:09:18 +0200

    Initial commit

---
+++ Assets/Scripts/Player/PlayerMovment.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody;
+
+    public float speed;
+    public Vector2 moveToPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveToPos.x = transform.position.x + Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        moveToPos.y = transform.position.y + Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+
+        rigidbody.MovePosition(moveToPos);
+    }
+}
